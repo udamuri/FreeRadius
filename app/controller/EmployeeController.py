a4a274f60b8e67afac08b32aa1e6e0328bfcabe5
Muri Budiman <muri.amscloud@co.id>
2023-10-17 15:14:00 +0700

    add freeradius db

--- app/controller/EmployeeController.py
+++ app/controller/EmployeeController.py
@@ -1,7 +1,20 @@
 from app import app, response
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+import pymysql.cursors
+connection = pymysql.connect(host=str(os.environ.get("DB_HOST")),
+                             user=str(os.environ.get("DB_USERNAME")),
+                             password=str(os.environ.get("DB_PASSWORD")),
+                             db=str(os.environ.get("DB_DATABASE")),
+                             charset='utf8mb4',
+                             cursorclass=pymysql.cursors.DictCursor)
 
 def index():
     try:
-        return response.ok([], "Hello World")
+        with connection.cursor() as cursor:
+            sql = "SELECT * FROM `employee`"
+            cursor.execute(sql)
+            result = cursor.fetchall()
+            return response.ok(result, "Hello World")
     except Exception as e:
-        return e
+        return response.badRequest(e, "Bad Request")
